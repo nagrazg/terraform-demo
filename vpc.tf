fbdb029844b110df276ec4b66816159b40c0b7d8
Nagaraju <43872253+nagrazg@users.noreply.github.com>
2023-03-23 17:42:48 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "name" {
   tags_all = var.default_tags
   tags = {
-    "name" = "$(var.default_tags.key1)-vpc"
+    name = "$(var.default_tags.key1)-vpc"
   }
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -10,7 +10,7 @@ resource "aws_vpc" "name" {
 
 resource "aws_subnet" "pbsubnet" {
   tags = {
-    "name" = "$(var.default_tags[key2])-vpc-(count.index)"
+    name = "$(var.default_tags[key2])-vpc-(count.index)"
   }
   vpc_id     = aws_vpc.name.id
   count =(var.pbsubnetcount > var.azcount ? var.pbsubnetcount % var.azcount: var.pbsubnetcount.index )
