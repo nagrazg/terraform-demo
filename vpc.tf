afaddacfce350f82190181b1d4881e9ec46a4aaa
Nagaraju <43872253+nagrazg@users.noreply.github.com>
2023-03-23 20:53:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,10 +11,10 @@ resource "aws_vpc" "name" {
 resource "aws_subnet" "pbsubnet" {
   vpc_id     = aws_vpc.name.id
   count = var.subnetcount
-   scount = (count.index <=3 ? count.index : count.index % 3  )
-  cidr_block = cidrsubnet("10.0.0.0/16",4,scount)
+  var.scount = (count.index <=3 ? count.index : count.index % 3  )
+  cidr_block = cidrsubnet("10.0.0.0/16",4,var.scount)
    tags = {
-    Name = "${var.default_tags[key2]}-${count.index}"
+    Name = "${var.default_tags["key2"]}- count.index"
   }
 }
 
