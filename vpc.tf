503ee3a0a7ea1cda7b91755317898f58bb00d873
Nagaraju <43872253+nagrazg@users.noreply.github.com>
2023-03-25 15:28:27 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,16 +21,31 @@ cidr_block = cidrsubnet("10.0.0.0/20", 4, count.index)
     }
 }
 
-
-
-
-
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.name.id
-
-  tags = {
-    Name = "main"
-    Project ="${var.default_tags.key1}"
+resource "aws_lb_listener" "lbl" {
+  load_balancer_arn = aws_lb.lb.arn
+  port = 80
+  protocol = "http"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.demo.arn
+    }
   }
+
+resource "aws_lb_target_group" "demo" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.name.id
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.demo.arn
+  target_id        = aws_instance.test.id
+  port             = 80
+}
+
+resource "aws_instance" "test" {
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = "t3.micro"
+  subnet_id = aws.pbsubnet[0].id
 }
