7fdd80b4b6233ab87c4b27907051e4b0cd5e472d
Nagaraju <43872253+nagrazg@users.noreply.github.com>
2023-03-25 15:36:28 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,8 +21,15 @@ cidr_block = cidrsubnet("10.0.0.0/20", 4, count.index)
     }
 }
 
-resource "aws_lb_listener" "lbl" {
-  load_balancer_arn = aws_lb.lb.arn
+resource "aws_lb" "demolb" {
+  load_balancer_type = "application"
+  internal = false
+  subnets = var.aws_subnet.pbsubnet.*.id 
+  name = "${var.default_tags["key2"]}"
+}
+
+resource "aws_lb_listener" "demolbl" {
+  load_balancer_arn = aws_lb.demolb.arn
   port = 80
   protocol = "http"
   default_action {
@@ -45,7 +52,6 @@ resource "aws_lb_target_group_attachment" "test" {
 }
 
 resource "aws_instance" "test" {
-  ami                     = data.aws_ami.ubuntu.id
   instance_type           = "t3.micro"
-  subnet_id = aws.pbsubnet[0].id
+  subnet_id = aws_subnet.pbsubnet[0].id
 }
