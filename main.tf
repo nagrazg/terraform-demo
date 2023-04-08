5d9aebae64f0dc19ca68dfbcbd233fa884779ffc
Nagaraju <43872253+nagrazg@users.noreply.github.com>
2023-04-08 16:08:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,9 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  
+   endpoints {
+    ec2            = "http://localhost:4566"
+  }
 
 }
